4ccb036c569d8009ba31d4774e1fb81f3252dff4
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-03-18 22:34:17 +0800

    comment out signin in nav bar

--- post.php
+++ post.php
@@ -24,7 +24,7 @@
         <li><a href="about.php">About</a></li>
         <li><a href="services.php">Services</a></li>
         <li><a href="contact.php">Contact</a></li>
-        <li><a href="signin.php">Signin</a></li>
+        <!-- <li><a href="signin.php">Signin</a></li> -->
         <li class = "navProfile">
           <div class="avatar">
             <img src="./images/avatar1.jpg" alt="avatar">
---
+++ signup.php
@@ -0,0 +1,9 @@
+<section class="formSection">
+  <div class="container formSectionContaine">
+    <h2>Sign Up</h2>
+    <div class="alertMessage error">
+      <p>This is an test error message</p>
+    </div>
+    <form action=""></form>
+  </div>
+</section>
