5ca47f7b30fc7f6d2d696adfafd4029ac298e049
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-03-21 21:17:27 +0800

    add signin and signup page

--- signin.php
+++ signin.php
@@ -17,23 +17,15 @@
 <body>
   <section class="formSection">
     <div class="container formSectionContainer">
-      <h2>Sign Up</h2>
-      <div class="alertMessage error">
-        <p>This is an test error message</p>
+      <h2>Sign In</h2>
+      <div class="alertMessage success">
+        <p>This is a test success message</p>
       </div>
       <form action="">
-        <input type="text" placeHolder="First Name">
-        <input type="text" placeHolder="Last Name">
-        <input type="text" placeHolder="Username">
-        <input type="email" placeHolder="Email">
-        <input type="password" placeHolder="Create Password">
-        <input type="password" placeHolder="Confirm Password">
-        <div class="formControl">
-          <label for="avatar" >User Avatar</label>
-          <input type="file" id="avatar">
-        </div>
-        <button type="submit" class="btn">Sign Up Now ! ! !</button>
-        <small>Already have an account ? <a href="signin.php">Sign In</a></small>
+        <input type="text" placeHolder="Username or Email">
+        <input type="password" placeHolder="Password">
+        <button type="submit" class="btn">Sign In</button>
+        <small>Don't have account ? <a href="signup.php">Sign Up</a></small>
       </form>
     </div>
   </section>  
