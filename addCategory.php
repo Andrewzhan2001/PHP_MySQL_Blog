54fb630ee3dac7897fb7f5066c823394374e2bbb
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-03-22 20:45:47 +0800

    finish addCategory and addPost page

--- addCategory.php
+++ addCategory.php
@@ -17,23 +17,14 @@
 <body>
   <section class="formSection">
     <div class="container formSectionContainer">
-      <h2>Sign Up</h2>
+      <h2>Add Category</h2>
       <div class="alertMessage error">
-        <p>This is an test error message</p>
+        <p>This is a test error message</p>
       </div>
       <form action="" enctype="multipart/form-data">
-        <input type="text" placeHolder="First Name">
-        <input type="text" placeHolder="Last Name">
-        <input type="text" placeHolder="Username">
-        <input type="email" placeHolder="Email">
-        <input type="password" placeHolder="Create Password">
-        <input type="password" placeHolder="Confirm Password">
-        <div class="formControl">
-          <label for="avatar" >User Avatar</label>
-          <input type="file" id="avatar">
-        </div>
-        <button type="submit" class="btn">Sign Up Now ! ! !</button>
-        <small>Already have an account ? <a href="signin.php">Sign In</a></small>
+        <input type="text" placeHolder="Title">
+        <textarea rows="4" placeholder="Description"></textarea>
+        <button type="submit" class="btn">Add Category</button>
       </form>
     </div>
   </section>  
